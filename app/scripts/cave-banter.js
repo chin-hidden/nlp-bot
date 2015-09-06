003824e512ac1d7fbb71891a74e9297317491016
Dave Bui <dung.bui@vndirect.com.vn>
2015-09-06 15:43:49 +0700

    Banterz.

--- app/scripts/cave-banter.js
+++ app/scripts/cave-banter.js
@@ -1,7 +1,22 @@
+import _ from "underscore";
+
+var bantz = [
+    { phrases: ['xinh qua', 'dep qua'], response: 'Hi hi, cám ơn quý khách. Em có thể giúp gì cho quý khách ạ?' },
+    { phrases: ['nguoi that'], response: 'Quý khách nghĩ em là ảo thì em là ảo, quý khách nghĩ em là thật thì em là thật ạ, hi hi.' },
+    { phrases: ['ngu the', 'do ngu'], response: 'Trí tuệ của em không thể so sánh được với quý khách ạ.' }
+];
 
 module.exports = {
     checkBanter: function(msg) {
-        
-        return false;
+        var response = false;
+        bantz.forEach(function(bant, i) {
+            bant.phrases.forEach(function(phrase, j) {
+                if (msg.search(phrase) > -1) {
+                    response = bant.response;
+                    return;
+                }
+            });
+        })
+        return response;
     }
 }
